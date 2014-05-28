f163f76055b366403273a7a9defefb9026bcd23e
Josh Ballesteros <josh@sqor.com>
2014-05-28 09:52:59 -0700

    adding crawler

--- crawler.py
+++ crawler.py
@@ -11,12 +11,18 @@ Webcrawler/scraper that extracts news stories
 from BeautifulSoup import BeautifulSoup
 
 #Step 1 read in yahoo file
+#copied from 
+#https://docs.python.org/2/tutorial/inputoutput.html
+lines = []
+f = open('webfiles/yahoo_story.html')
+for html_lines in f:
+    lines.append(html_lines)
 
-
+print len(lines)
 #Step 2 extract paragraph from file
+#use Beautiful soup on lines list and extract it
 
 
 #Step 3 write out paragraph to file out.txt
-
-
+#write out extracted text to out.txt
 
